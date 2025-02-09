c8c98dd9728ab76d3807eb505e99efa6ac01d92b
aldilukmn <aldilukman99@gmail.com>
2025-02-09 22:25:42 +0700

    update: add cookie parser

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import userRoutes from './routes/user.route';
 import env from 'dotenv';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(express.urlencoded({ extended: true })); //For Login body json, POST METHOD
 env.config();
+app.use(cookieParser());
 app.use(cors({
   credentials: true,
   origin: true,
