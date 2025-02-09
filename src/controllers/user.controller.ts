dea392d51f953a934fbba1f474bf155f7ed4a8fe
aldilukmn <aldilukman99@gmail.com>
2025-02-09 22:08:26 +0700

    update: add some site none

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -42,7 +42,7 @@ export class UserController {
         httpOnly: true,
         maxAge: 60 * 60 + 1000,
         secure: true,
-        sameSite: 'lax'
+        sameSite: 'none'
       });
       res.status(200).json(response);
     } catch (e) {
