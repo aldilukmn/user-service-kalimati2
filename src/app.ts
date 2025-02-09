6783cf7b5afe769f4c386fcbd246ebe9ec71870b
aldilukmn <aldilukman99@gmail.com>
2025-02-09 21:46:39 +0700

    fix: cors handle cookie 2

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import userRoutes from './routes/user.route';
 import env from 'dotenv';
+import cors from 'cors';
 
 const app = express();
 app.use(express.urlencoded({ extended: true })); //For Login body json, POST METHOD
 env.config();
+app.use(cors({
+  credentials: true,
+}))
 app.use('/api/users', userRoutes)
 
 export default app;
