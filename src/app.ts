ca8b978987b5b0dc3da06eea1868bbe4d07a72d9
aldilukmn <aldilukman99@gmail.com>
2025-02-09 21:41:53 +0700

    fix: cors handle cookie

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,10 @@
 import express from 'express';
 import userRoutes from './routes/user.route';
 import env from 'dotenv';
-import cors from 'cors';
 
 const app = express();
 app.use(express.urlencoded({ extended: true })); //For Login body json, POST METHOD
 env.config();
-app.use(cors({
-  origin: ['http://localhost:3000', 'https://uptdsdn2kalimati.vercel.app/'],
-  credentials: true
-}))
 app.use('/api/users', userRoutes)
 
 export default app;
