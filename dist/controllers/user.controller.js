bc1c682ea2507c96ebad40b73bee96e2c39d57d9
aldilukmn <aldilukman99@gmail.com>
2025-02-13 23:19:17 +0700

    update: add same-site lax

--- dist/controllers/user.controller.js
+++ dist/controllers/user.controller.js
@@ -57,6 +57,7 @@ class UserController {
                     httpOnly: true,
                     maxAge: 60 * 60 + 1000,
                     secure: process.env.NODE_ENV === 'production',
+                    sameSite: 'lax'
                 }).status(200).json(response);
             }
             catch (e) {
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -42,6 +42,7 @@ export class UserController {
         httpOnly: true,
         maxAge: 60 * 60 + 1000,
         secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax'
       }).status(200).json(response);
     } catch (e) {
       if (e instanceof Error) {
