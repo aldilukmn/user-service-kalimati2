78fc8d609bc20b64c067b9aae7e3bda5d850315a
aldilukmn <aldilukman99@gmail.com>
2025-02-13 23:39:20 +0700

    update: change none of samesite

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -42,7 +42,7 @@ export class UserController {
         httpOnly: true,
         maxAge: 60 * 60 + 1000,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'lax',
+        sameSite: 'none',
         path: '/'
       }).status(200).json(response);
     } catch (e) {
@@ -59,7 +59,7 @@ export class UserController {
       res.clearCookie(`${process.env.COOKIE_NAME}` as string, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'lax',
+        sameSite: 'none',
         path: '/'
       })
       res.status(200).json(response);
