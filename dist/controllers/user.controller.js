890e589a274b4b6d0f043f9292793713f60c75b6
aldilukmn <aldilukman99@gmail.com>
2025-02-23 11:19:32 +0700

    update: path change

--- dist/controllers/user.controller.js
+++ dist/controllers/user.controller.js
@@ -58,7 +58,7 @@ class UserController {
                     maxAge: 60 * 60 * 1000,
                     secure: process.env.NODE_ENV === 'production',
                     sameSite: 'none', //with secure is active,
-                    path: '/api/user/',
+                    path: '/',
                     domain: 'uptdsdn2kalimati.vercel.app'
                 }).status(200).json(response);
             }
@@ -80,7 +80,7 @@ class UserController {
                     httpOnly: true,
                     secure: process.env.NODE_ENV === 'production',
                     sameSite: 'none', //with secure is active,
-                    path: '/api/user/',
+                    path: '/',
                     domain: 'uptdsdn2kalimati.vercel.app'
                 }).status(200).json(response);
             }
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -43,7 +43,7 @@ export class UserController {
         maxAge: 60 * 60 * 1000,
         secure: process.env.NODE_ENV === 'production',
         sameSite: 'none', //with secure is active,
-        path: '/api/user/',
+        path: '/',
         domain: 'uptdsdn2kalimati.vercel.app'
       }).status(200).json(response);
     } catch (e) {
@@ -61,7 +61,7 @@ export class UserController {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         sameSite: 'none', //with secure is active,
-        path: '/api/user/',
+        path: '/',
         domain: 'uptdsdn2kalimati.vercel.app'
       }).status(200).json(response);
     } catch (e) {
