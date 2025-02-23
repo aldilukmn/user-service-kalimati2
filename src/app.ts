faac7bc49db6a28fc1a81029aa23b11c60eff8e3
aldilukmn <aldilukman99@gmail.com>
2025-02-23 11:16:29 +0700

    update: add expose header

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,12 @@ import cookieParser from 'cookie-parser';
 const app = express();
 app.use(express.urlencoded({ extended: true })); //For Login body json, POST METHOD
 env.config();
+app.use(cookieParser());
 app.use(cors({
   origin: [`${process.env.FE_URL}`, 'http://localhost:3000'],
   methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
   credentials: true,
+  exposedHeaders: ['Set-Cookie']
 }));
-app.use(cookieParser());
 app.use('/api/users', userRoutes);
 export default app;
