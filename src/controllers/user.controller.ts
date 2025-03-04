00b1ecf12c06b09730e667e298ee089a379c6ee2
aldilukmn <aldilukman99@gmail.com>
2025-03-04 21:38:24 +0700

    update: change domain

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -44,7 +44,7 @@ export class UserController {
         secure: process.env.NODE_ENV === 'production',
         sameSite: 'none', //with secure is active,
         path: '/',
-        domain: ".netlify.app",
+        // domain: ".netlify.app",
         // domain: 'uptdsdn2kalimati.vercel.app'
       }).status(200).json(response);
     } catch (e) {
@@ -63,7 +63,7 @@ export class UserController {
         secure: process.env.NODE_ENV === 'production',
         sameSite: 'none', //with secure is active,
         path: '/',
-        domain: ".netlify.app",
+        // domain: ".netlify.app",
         // domain: 'uptdsdn2kalimati.vercel.app'
       }).status(200).json(response);
     } catch (e) {
